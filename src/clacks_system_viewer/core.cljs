649df765f366637c4f806445bdf220e92e034f4d
John Stevenson <john@jr0cket.co.uk>
2016-10-12 03:49:45 +0100

    Simple cards - html content from sablono
    Two simple cards that contain only html generated by the Sablono
    library.

--- src/clacks_system_viewer/core.cljs
+++ src/clacks_system_viewer/core.cljs
@@ -7,9 +7,22 @@
 
 (enable-console-print!)
 
-(defcard first-card
+;; Simple content can be created without a card frame (heading) by not including
+;; a name for a card.  In this case the card simply displays the text of the string.
+;; This text will process markdown symbols such as # for heading styles
+(defcard
+  "# A Clacks Tower system visualised by DevCards")
+
+;; A card can include simple text or html content.
+;; The sablono library generates html for you, using Clojure keywords & vectors
+;; to represent the structure of the html you require.
+(defcard introduction
   (sab/html [:div
-             [:h1 "This is your first devcard!"]]))
+             [:h1 "Overview of the Clacks System Viewer"]
+             [:p "Using the Sablono library makes it much easier to create your html
+                  structure without having to manage html tags"]
+             [:p "The inital design will be to define a devcard for each clacks tower
+                  in the system"]]))
 
 (defn main []
   ;; conditionally start the app based on whether the #main-app-area
